857181ed48556ad29ce59a8add31abe4faed2529
DanielTheRev <fernando.larrosa94@gmail.com>
2023-04-20 00:01:16 -0300

    agregado pwa con exito

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // server_uri: 'http://192.168.100.13:3000'
-  server_uri: 'https://experienciasmemorableseventos.com.ar'
+  server_uri: 'http://192.168.100.13:3000'
+  // server_uri: 'https://experienciasmemorableseventos.com.ar'
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app/app.component';
 import { AppRoutes } from './app/app.routes';
@@ -11,6 +12,14 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(AppRoutes),
     importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
+      })
+    ),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
 });
