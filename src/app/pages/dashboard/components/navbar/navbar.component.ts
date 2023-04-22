59d61094e2e50b28f3470eec00f0c6c23270294c
DanielTheRev <fernando.larrosa94@gmail.com>
2023-04-22 01:24:27 -0300

    opmitmizando interfaz para moviles

--- src/app/pages/dashboard/components/navbar/navbar.component.ts
+++ src/app/pages/dashboard/components/navbar/navbar.component.ts
@@ -1,35 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { faImages, faUser, faNewspaper, faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+
   navs = [
     {
       link: 'Imagenes-fondo',
       activeClass: 'active',
-      name: 'Fondo de imagenes'
+      name: 'Fondo de imagenes',
+      icon: faImages
     },
     {
       link: 'nosotros',
       activeClass: 'active',
-      name: 'Nosotros'
+      name: 'Nosotros',
+      icon: faUser
     },
     {
       link: 'servicios',
       activeClass: 'active',
-      name: 'Servicios'
+      name: 'Servicios',
+      icon: faNewspaper
     },
     {
       link: 'contacto',
       activeClass: 'active',
-      name: 'Contacto'
+      name: 'Consultas',
+      icon: faIdCard
     },
   ]
 }
